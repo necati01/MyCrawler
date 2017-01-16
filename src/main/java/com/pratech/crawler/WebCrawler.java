0620cb0152c7485ea1751d23d92a21eb433b24e8
necatitok <necati.tok@enacore.com.tr>
2017-01-16 00:25:19 +0000

    internal urls checked

--- src/main/java/com/pratech/crawler/WebCrawler.java
+++ src/main/java/com/pratech/crawler/WebCrawler.java
@@ -1,12 +1,26 @@
 package com.pratech.crawler;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WebCrawler {
-
-	public static Document getPage(String url) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public WebCrawler(){
+	}
+	
+	public void crawl(Domain domain, Document document, String url) {
+		String baseUrl=domain.getBaseUrl();
+		Elements links = document.select("a[href]");
+		for (Element link : links) {
+			String href = link.attr("abs:href");
+			if (href.startsWith(baseUrl)) {
+				domain.getDomainUrls().add(href);
+			} else if (!domain.getExternalUrls().contains(href)) {
+				domain.getExternalUrls().add(href);
+			}
+		}
+		
 	}
 
 }
--- src/test/java/com/pratech/crawler/WebCrawlerTest.java
+++ src/test/java/com/pratech/crawler/WebCrawlerTest.java
@@ -1,24 +1,56 @@
 package com.pratech.crawler;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Before;
 import org.junit.Test;
 
 public class WebCrawlerTest {
-
+	Document doc = null; 
+	
 	@Before
 	public void init() throws IOException {
 	}
 
     @Test 
-    public void getPageTest() throws IOException { 
-    	String url="www.google.com";
-    	Document doc=WebCrawler.getPage(url);
-		assertTrue(doc.title().contains("Google")); 
+    public void getPageTest() throws IOException , IllegalArgumentException { 
+    	String url="https://www.google.com";
+    	doc=WebCrawlerUtil.getPage(url);
+		assertTrue(doc.title().contains("Google"));
     }
-	
+    
+    @Test 
+    public void getDomainLinks() throws IOException , IllegalArgumentException {
+    	StringBuffer html = new StringBuffer("<a href=\"http://pratechnology.co.uk\"></a>"); 
+    	String host = "http://pratechnology.co.uk"; 
+    	Domain domain=new Domain(host);
+    	String expectedUrl = "http://pratechnology.co.uk";
+    	setDoc(html.toString(), host);
+    	WebCrawler crawler =new WebCrawler();
+    	crawler.crawl(domain,doc,host);
+    	assertThat(domain.getDomainUrls(), hasItem(expectedUrl));
+    	
+    	html.append("<a href=\"http://www.abcdef.co.uk\"></a>");
+    	html.append("<a href=\"http://pratechnology.co.uk/company\"></a>"); 
+    	Domain domain2=new Domain(host);
+    	setDoc(html.toString(), host);
+    	crawler =new WebCrawler();
+    	crawler.crawl(domain2,doc,host);
+    	assertThat(domain2.getDomainUrls(), hasItem("http://pratechnology.co.uk"));
+    	assertThat(domain2.getDomainUrls(), hasItem("http://pratechnology.co.uk/company"));
+    	assertTrue(!domain2.getDomainUrls().contains("http://www.abcdef.co.uk"));
+    }
+    
+    private void setDoc(String html, String host) { 
+    	  String htmlSkeleton = "<html><head></head><body></body></html>"; 
+    	  this.doc = Jsoup.parse(htmlSkeleton, "http://" + host); 
+    	  this.doc.body().append(html);   
+    	  this.doc.normalise(); 
+   } 
 }
